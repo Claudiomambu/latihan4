a6490ea75ac6250a4ffa271fc7792aefb48adf6c
ClaudioMambu <s11810048@student.unklab.ac.id>
2021-03-11 11:41:35 +0700

    latihan 4 mopro

---
+++ array.js
@@ -0,0 +1,12 @@
+//Array4
+
+//let numbers = [1, 2, 3, 4, 5];
+//let john = ('John', 'Doe', 33, true);
+
+//let numbers = new Array(1, 2, 3 ,4 ,5);
+//let john = new Array ('John', 'Doe', 33, true);
+
+//john[3] = false;
+
+//console.log(numbers[numbers.length - 1]);
+//console.log(john[john.length - 1]);
---
+++ object.js
@@ -0,0 +1,28 @@
+//// Mobile Application Development A \\\\
+// Claudio Mambu \\
+//Object4
+
+// const john = {
+//     firstName: 'John',
+//    lastName: 'Doe',
+//    age: 33,
+//    isMarried: true,
+//    sayHello: function () {
+//        console.log("Hello");
+//    },
+// };
+
+//const john = new Object();
+//john.firstName = 'John';
+//john.lastName = "Doe";
+//john.age = 33;
+//john.isMarried = true;
+
+// console.log(john.lastName);
+// console.log(john["isMarried"]);
+
+// john.address = "Manado";
+// john["nationality"] = "Indonesia";
+// console.log(john);
+
+// john.sayHello();
